6b47e48d613964bde26184137ec5a0cf0c4f66fb
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-05-14 14:00:31 +0200

    report table logic

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cholera;
+
+class ReportController extends Controller
+{
+    public function index()
+    {
+        return view('reports.index');
+    }
+}
